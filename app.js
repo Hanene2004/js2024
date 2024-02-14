32b57cbff0d66104d1979bdb25221dae28e3abf6
Hanene2004 <149962455+Hanene2004@users.noreply.github.com>
2024-02-14 23:09:05 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+function max2(a,b)
+{
+    if (a>b)
+    return a
+    else
+    return b
+}
+console.log(max2(5,8))
+console.log(max2(15,8))
+console.log(max2(15,15))
+function som2(a,b)
+{
+    return (a+b)
+}
+console.log(som2(5,8))
+function sommeDeuxInputs() {
+    let a = Number(prompt("Entrez le premier nombre:"));
+    let b = Number(prompt("Entrez le deuxième nombre:"));
+    alert("La somme est " + (a + b));
+  }
+  
